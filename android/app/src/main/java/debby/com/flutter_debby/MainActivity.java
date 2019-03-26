f081ac0bb1d1d38744216bc329ee459ef0826dd6
wangkaixun <298270773@qq.com>
2019-03-26 09:57:41 +0800

    第二次提交

---
+++ android/app/src/main/java/debby/com/flutter_debby/MainActivity.java
@@ -0,0 +1,13 @@
+package debby.com.flutter_debby;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
